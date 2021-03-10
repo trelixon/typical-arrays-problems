bc121fc7ac1ce132cd2c499f1c5b3adefbad6d82
trelixion <60671130+trelixon@users.noreply.github.com>
2021-03-10 02:21:48 +0200

    feat: index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+    if (array==undefined || array==0) {
+        return 0;
+    }else{
+let mina = Math.min.apply(null, array);
+return mina;}
+    };
 
 exports.max = function max (array) {
-  return 0;
-}
+  if (array==undefined || array==0) {
+        return(0);
+    }else{
+    let maxa = Math.max.apply(null, array);
+    return maxa;
+    }
+};
 
 exports.avg = function avg (array) {
-  return 0;
-}
+  if (array===undefined || array.length===0) {
+    return 0;}
+  
+  let balanc=0;
+  for (let i=0; i < array.length;i++){
+    balanc += array[i];} 
+    return balanc/array.length;};
