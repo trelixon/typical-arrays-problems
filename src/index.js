440894542822893de8acb9e0ac253c89752e4edf
trelixion <60671130+trelixon@users.noreply.github.com>
2021-03-10 02:23:49 +0200

    fix:index.html

--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
 
 exports.min = function min (array) {
     if (array==undefined || array==0) {
-        return 0;
+        return(0);
     }else{
-let mina = Math.min.apply(null, array);
-return mina;}
+let mina=Math.min.apply(null, array);
+return (mina);}
     };
 
 exports.max = function max (array) {
   if (array==undefined || array==0) {
         return(0);
     }else{
-    let maxa = Math.max.apply(null, array);
-    return maxa;
+    let maxa=Math.max.apply(null,array);
+    return(maxa);
     }
 };
 
 exports.avg = function avg (array) {
   if (array===undefined || array.length===0) {
-    return 0;}
+    return(0);}
   
   let balanc=0;
-  for (let i=0; i < array.length;i++){
-    balanc += array[i];} 
-    return balanc/array.length;};
+  for (let i=0;i<array.length;i++){
+    balanc+=array[i];} 
+    return(balanc/array.length);};
